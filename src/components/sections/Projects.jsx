68116081fc4aa769e792365ad224d644b35caf33
Eric Lin <el3136@nyu.edu>
2025-06-07 21:56:04 -0400

    Add Project 3

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -69,6 +69,36 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+
+            {/* Project 3 */}
+            <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition'>
+              <h3 className='text-xl font-bold mb-2 '> Multimodal_Video_Analysis </h3>
+              <p className='text-gray-400 mb-4'> 
+                This is an AI video assistant that helps you extract information from YouTube content without
+                watching the entire video. It processes video transcripts to create an interactive experience
+                where you can ask questions, get summaries, obtain timestamps, and explore the video content
+                through a chatbot.
+              </p>
+
+              <div className='flex flex-wrap gap-2 mb-4'>
+                {["Next.js", "Typescript", "Cartesia", "Vercel", "OpenAI Whisper", "Groq"].map((tech, key) => (
+                  <span 
+                    key={key}
+                    className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                              hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all'
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className='flex justify-between items-center'>
+                <a href='https://github.com/el3136/Property-Recommendation-System' className='text-blue-400 hover:text-blue-300 transition-colors my-4'> 
+                  {" "}
+                  View Project 
+                </a>
+              </div>
+            </div>
             {/* Next Project */}
             
 
